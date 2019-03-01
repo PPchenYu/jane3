ab163662b22299f3b9aadea255695a8efe775132
CRYstaL_Jane <409622462@qq.com>
2019-03-01 16:34:41 +0800

    add line10

---
+++ gittest/src/main/java/cn/itlcy/git/HelloGit.java
@@ -0,0 +1,10 @@
+package cn.itlcy.git;
+
+public class HelloGit {
+    public static void main(String[] args) {
+
+        System.out.println("Hey Baby_Jane ~~~");
+        System.out.println("Crystal fall in love with you ~~~");
+
+    }
+}
