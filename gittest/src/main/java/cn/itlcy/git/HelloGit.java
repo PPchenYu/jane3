7fb091664105159980203f26129a6572f6ebbd02
CRYstaL_Jane <409622462@qq.com>
2019-03-01 17:03:25 +0800

    update line7

--- gittest/src/main/java/cn/itlcy/git/HelloGit.java
+++ gittest/src/main/java/cn/itlcy/git/HelloGit.java
@@ -4,7 +4,7 @@ public class HelloGit {
     public static void main(String[] args) {
 
         System.out.println("Hey Baby_Jane ~~~");
-        System.out.println("Crystal fall in love with you ~~~");
+        System.out.println("Crystal fall in love with you all my life~~~");
 
     }
 }
